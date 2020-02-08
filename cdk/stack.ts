4c615c17ba20c01476a6059f1e5d99e9813632e3
Jordan Schroter <github@jschr.io>
2020-02-07 23:52:42 -0500

    Swap terraform for aws-cdk

---
+++ cdk/stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export default class extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
