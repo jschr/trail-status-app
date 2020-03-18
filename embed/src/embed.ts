d0b1ea93121c538a964f1e476f64fe5403ccc9f8
Jordan <github@jschr.io>
2020-03-18 08:43:08 -0400

    Embed improvements

--- embed/src/embed.ts
+++ embed/src/embed.ts
@@ -2,16 +2,32 @@ import 'classlist-polyfill';
 import 'promise/polyfill';
 import 'whatwg-fetch';
 import './embed.css';
+import * as timeago from 'timeago.js';
 
 const trailStatusApi = process.env.API_ENDPOINT;
 const trailStatusIds: { [key: string]: true } = {};
 const trailStatusAttribute = 'data-trail-status';
+
+const ensureImage = async (src: string) =>
+  new Promise(resolve => {
+    const img = new Image();
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+    img.src = src;
+  });
 
 export const fetchTrailStatus = async (trailId: string) => {
   const container = document.querySelector(
     `[${trailStatusAttribute}="${trailId}"]`,
   );
   if (!container) throw new Error(`Failed to find DOM node for '${trailId}`);
+
+  const imageEl = container.querySelector('.trailStatusImage');
+  const messageEl = container.querySelector('.trailStatusMessage');
+  const ctaEl = container.querySelector('.trailStatusCta');
+  const timeagoEl = container.querySelector('.trailStatusTimeago');
+
+  if (!imageEl || !messageEl || !timeagoEl || !ctaEl) return;
 
   const resp = await fetch(
     `${trailStatusApi}/status?trailId=${encodeURIComponent(trailId)}`,
@@ -29,6 +45,8 @@ export const fetchTrailStatus = async (trailId: string) => {
   }
 
   const trailStatus = await resp.json();
+  if (!trailStatus || !trailStatus.user) return;
+
   if (trailStatus.status === 'open') {
     container.classList.remove('trailStatus-isClosed');
     container.classList.add('trailStatus-isOpen');
@@ -37,16 +55,27 @@ export const fetchTrailStatus = async (trailId: string) => {
     container.classList.add('trailStatus-isClosed');
   }
 
-  const message = container.querySelector('.trailStatusMessage');
-  if (!message) return;
+  const imagesExists = await ensureImage(trailStatus.imageUrl);
+  if (imagesExists && imageEl && trailStatus.imageUrl) {
+    container.classList.add('trailStatus-hasImage');
+    imageEl.setAttribute(
+      'style',
+      `background-image: url(${trailStatus.imageUrl})`,
+    );
+  } else if (imageEl) {
+    container.classList.remove('trailStatus-hasImage');
+    imageEl.removeAttribute('style');
+  }
 
-  message.textContent = trailStatus.message;
+  messageEl.textContent = trailStatus.message;
 
-  const cta = container.querySelector('.trailStatusCta');
-  if (!cta || !trailStatus.user) return;
+  timeagoEl.setAttribute('datetime', trailStatus.updatedAt);
+  timeago.render(timeagoEl as HTMLElement);
 
   const { username } = trailStatus.user;
-  cta.innerHTML = `Follow us at <a href="https://www.instagram.com/${username}/" target="_black">@${username}</a> for trail updates.`;
+  ctaEl.innerHTML = `Follow us at <a href="https://www.instagram.com/${username}/" target="_black">@${username}</a> for trail updates.`;
+
+  container.classList.add('trailStatus-isLoaded');
 };
 
 export const register = () => {
@@ -62,8 +91,17 @@ export const register = () => {
 
       container.innerHTML = [
         `<div class="trailStatusInner">`,
+        `<div class="trailStatusCallout">`,
+        `<div class="trailStatusImage"></div>`,
+        `<div class="trailStatusInfo">`,
         `<div class="trailStatusMessage"></div>`,
+        `<div class="trailStatusTimestamp">`,
+        `Updated <span class="trailStatusTimeago"></span>.`,
+        `</div>`,
+        `</div>`,
+        `</div>`,
         `<div class="trailStatusCta"></div>`,
+        `</div>`,
         `</div>`,
       ].join('');
 
