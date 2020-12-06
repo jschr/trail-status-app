bf93bfa00079016afa6fe6a58a170408758a3024
Jordan <github@jschr.io>
2020-12-06 15:11:07 -0500

    Automatically reload app if launching from homescreen

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,6 +7,22 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+// Automatically reload app if launching from homescreen.
+const isInWebAppiOS = (window.navigator as any).standalone === true;
+const isInWebAppChrome = window.matchMedia('(display-mode: standalone)')
+  .matches;
+
+if (isInWebAppiOS || isInWebAppChrome) {
+  const dateOpened = new Date();
+  window.addEventListener('focus', () => {
+    const now = new Date();
+    const oneMinute = 1000 * 60;
+    if (+now - +dateOpened > oneMinute) {
+      window.location.reload();
+    }
+  });
+}
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
